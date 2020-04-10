77fda41b329edc561bd30a7dc1c0f5fae068dff0
spencer-mcguire <smcguire1826@gmail.com>
2020-04-10 12:06:55 -0500

    clean up print

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -47,7 +47,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(f'("{c.name}": {c.lat}, {c.lon})')
+    print(c)
 
 # STRETCH GOAL!
 #
